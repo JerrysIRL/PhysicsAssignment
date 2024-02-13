fe0339b1723e6777ba4c332512dd4a43eab87898
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2024-02-13 18:35:51 +0100

    Added ball trajectory

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(TrajectoryVisualizer))]
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float forceMulti = 3f;
+    [SerializeField] private float verticalForceMulti = 3f;
+    private TrajectoryVisualizer _trajectoryVisualizer;
+    private Rigidbody _rb;
+    private Vector3 _mousePressDownPos;
+    private Vector3 _mousePressUpPos;
+    private bool isShooting;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _trajectoryVisualizer = GetComponent<TrajectoryVisualizer>();
+    }
+
+    private void OnMouseDown()
+    {
+        _mousePressDownPos = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        _mousePressUpPos = Input.mousePosition;
+        Shoot(_mousePressDownPos- _mousePressUpPos );
+        _trajectoryVisualizer.ClearTrajectory();
+    }
+
+    private void OnMouseDrag()
+    {
+        if(isShooting)
+            return; 
+        Vector3 direction = Input.mousePosition - _mousePressDownPos;
+        Vector3 force = new Vector3(direction.x, direction.y * verticalForceMulti, direction.y) * forceMulti;
+        _trajectoryVisualizer.UpdateTrajectory(force, _rb, transform.position);
+    }
+
+    void Shoot(Vector3 force)
+    {
+        if (isShooting)
+        {
+            return;
+        }
+        
+        Vector3 myforce = new Vector3(force.x, force.y * verticalForceMulti, force.y) * forceMulti;
+        _rb.AddForce(myforce);
+        isShooting = true;
+    }
+}
---
+++ Assets/Scripts/TrajectoryVisualizer.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryVisualizer : MonoBehaviour
+{
+    [SerializeField] [Range(3, 30)] private int lineSegments = 20;
+    private readonly List<Vector3> _pointsList = new List<Vector3>();
+
+    private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Start is called before the first frame update
+    public void UpdateTrajectory(Vector3 forceVector, Rigidbody rb, Vector3 startingPoint)
+    {
+        Vector3 velocity = (forceVector / rb.mass) * Time.fixedDeltaTime;
+        float airDuration = (2 * velocity.y) / Physics.gravity.y;
+        float stepTime = airDuration / lineSegments;
+
+        _pointsList.Clear();
+        for (int i = 0; i < lineSegments; i++)
+        {
+            float stepTimePassed = stepTime * i;
+
+            Vector3 movementVector = new Vector3(
+                velocity.x * stepTimePassed,
+                velocity.y * stepTimePassed - 0.5f * Physics.gravity.y * stepTimePassed * stepTimePassed,
+                velocity.z * stepTimePassed
+            );
+
+            // if (Physics.Raycast(startingPoint, -movementVector, out RaycastHit hit, movementVector.magnitude))
+            // {
+            //     break;
+            // }
+            
+            _pointsList.Add(-movementVector + startingPoint);
+        }
+
+        _lineRenderer.positionCount = lineSegments;
+        _lineRenderer.SetPositions(_pointsList.ToArray());
+    }
+
+    public void ClearTrajectory()
+    {
+        _lineRenderer.positionCount = 0;
+    }
+}
